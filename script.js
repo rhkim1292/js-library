2ddd0c63707f32ab7d526b4f274096bf344c96eb
Randy Kim <rhkim1292@gmail.com>
2022-08-04 01:16:46 -0700

    Implement the general layout of the webpage
    Added code in the js file that prompts the user for their name in order
    to display the user's name in the header of the page.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const USER_NAME = prompt("What is your name?");
+
+const flexContainerH1 = document.querySelector("div.flex-container h1");
+flexContainerH1.textContent = `${USER_NAME}'s Book Library`;
+
 var myLibrary = [];
 
 function Book() {
