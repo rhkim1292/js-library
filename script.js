b1c0d0c49e90391093f82a25b69d288bc2e66321
Randy Kim <rhkim1292@gmail.com>
2022-08-05 01:58:35 -0700

    Give a basic style to the book card container
    Basic styling was given to the book card container in order to test
    the addition of a few test books in the user's library.
    
    There was also some beginning code added to the javascript file that
    will become the building blocks of implementing the functionality that
    will allow the user to add books with their own inputs.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const USER_NAME = prompt("What is your name?");
 
 const flexContainerH1 = document.querySelector("div.flex-container h1");
+const bookGrid = document.querySelector("div.flex-container div.book-grid");
 flexContainerH1.textContent = `${USER_NAME}'s Book Library`;
 
 var myLibrary = [];
 
-function Book() {
+function Book(author, title, numOfPages) {
     // the constructor
+    this.author = author;
+    this.title = title;
+    this.numOfPages = numOfPages;
+    this.read = false;
 }
 
 function addBookToLibrary() {
 }
+
+const testBook = new Book("rndi", "test", 1337);
+const newBookCard = document.createElement("div");
+const newBookTitle = document.createElement("h3");
+const newBookAuthor = document.createElement("p");
+const newBookPgCount = document.createElement("p");
+const newBookRead = document.createElement("p");
+newBookElement.classList.add("book-card");
+newBookElement.appendChild("")
+myLibrary.push(testBook);
+
+
+bookGrid.appendChild(newBookElement);
