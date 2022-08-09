285f115921e1e43d9cd4ca5f1c83a728c10f6790
Randy Kim <rhkim1292@gmail.com>
2022-08-09 03:29:06 -0700

    Enable displaying of books from the library array
    Books that were added to the myLibrary array via the new book form
    are now displayed in the book grid.
    
    A reloadLibraryDisplay() function has been implemented because I
    believe it's simplest to just have it be called whenever the myLibrary
    array is altered. For now, the only time it's called is when a book is
    removed from the array or when a new book is added.
    
    Whenever reloadLibraryDisplay() is called, the child elements of the
    book grid element need to be cleared and children need to be
    re-appended for each book in the library.
    
    createBookCardDOMElement() was implemented to make sure a DOM
    element is properly created for each book card element that needs to be
    displayed in the book grid. This function is called cleanly within
    reloadLibraryDisplay() for every book in the library.

--- script.js
+++ script.js
@@ -14,41 +14,95 @@ function Book(author, title, numOfPages, readFlag) {
 	this.author = author;
 	this.title = title;
 	this.numOfPages = numOfPages;
-	readFlag === null ? this.readFlag = false : this.readFlag = true;
+	readFlag === null ? (this.readFlag = false) : (this.readFlag = true);
 }
 
 function addBookToLibrary(formData) {
 	const newBook = new Book(
 		formData.get("author_name"),
 		formData.get("title_name"),
-        formData.get("num_of_pages"),
-        formData.get("read_flag")
+		formData.get("num_of_pages"),
+		formData.get("read_flag")
 	);
-    myLibrary.push(newBook);
+	myLibrary.push(newBook);
+}
+
+function removeBookFromLibrary(index) {
+	myLibrary.splice(index, 1);
+	reloadLibraryDisplay();
+}
+
+function reloadLibraryDisplay() {
+	bookGrid.innerHTML = ""; // Clear the book grid element of its children
+
+	// Then re-populate the book grid with book card elements
+	myLibrary.forEach((book, index) => {
+		bookGrid.appendChild(createBookCardDOMElement(book, index));
+	});
+}
+
+function createBookCardDOMElement(book, index) {
+	const newBookCard = document.createElement("div");
+	newBookCard.setAttribute("class", "book-card");
+
+	const newBookTitle = document.createElement("h3");
+	newBookTitle.setAttribute("class", "book-title");
+	newBookTitle.textContent = book.title;
+
+	const newBookAuthor = document.createElement("p");
+	newBookAuthor.setAttribute("class", "book-author");
+	newBookAuthor.textContent = `by ${book.author}`;
+
+	const newBookPgCount = document.createElement("p");
+	newBookPgCount.setAttribute("class", "book-pg-count");
+	newBookPgCount.textContent = `# of pages: ${book.numOfPages}`;
+
+	const newBookRead = document.createElement("p");
+	newBookRead.setAttribute("class", "book-read");
+	newBookRead.textContent = book.readFlag ? "Read?: Yes" : "Read?: No";
+
+	const newBookMarkReadBtn = document.createElement("button");
+	newBookMarkReadBtn.setAttribute("class", "book-read-btn");
+	newBookMarkReadBtn.textContent = book.readFlag
+		? "Mark as unread"
+		: "Mark as read";
+
+	const newBookDeleteBtn = document.createElement("button");
+	newBookDeleteBtn.setAttribute("class", "book-del-btn");
+	newBookDeleteBtn.setAttribute("onclick", `removeBookFromLibrary(${index})`);
+	newBookDeleteBtn.textContent = "Remove Book";
+
+	newBookCard.append(
+		newBookTitle,
+		newBookAuthor,
+		newBookPgCount,
+		newBookRead,
+		newBookMarkReadBtn,
+		newBookDeleteBtn
+	);
+
+	return newBookCard;
+}
+
+function enableForm() {
+	newBookBtn.style.display = "none";
+	form.style.display = "block";
+}
+
+function disableForm() {
+	newBookBtn.style.display = "block";
+	form.style.display = "none";
 }
 
 newBookBtn.addEventListener("mouseup", (e) => {
-	newBookBtn.style.display = "none";
-	form.style.display = "block";
+	enableForm();
 });
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const formData = new FormData(e.target);
 	addBookToLibrary(formData);
+	reloadLibraryDisplay();
 	form.reset();
-	newBookBtn.style.display = "block";
-	form.style.display = "none";
+	disableForm();
 });
-
-const testBook = new Book("rndi", "test", 1337);
-const newBookCard = document.createElement("div");
-const newBookTitle = document.createElement("h3");
-const newBookAuthor = document.createElement("p");
-const newBookPgCount = document.createElement("p");
-const newBookRead = document.createElement("p");
-newBookElement.classList.add("book-card");
-newBookElement.appendChild("");
-myLibrary.push(testBook);
-
-bookGrid.appendChild(newBookElement);
